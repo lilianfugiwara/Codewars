4fe754b1ef1a01b40a1e77164373814d8dda0160
lfugiwara@gmail.com <lfugiwara@gmail.com>
2021-03-04 18:30:43 +0000

    BitCount

---
+++ BitCounting/BitCounting/BitCounting.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitCounting
+{
+    [TestFixture]
+    public class BitCounting
+    {
+        [Test]
+        public void CountBits()
+        {
+            Assert.AreEqual(0, Kata.CountBits(0));
+            Assert.AreEqual(1, Kata.CountBits(4));
+            Assert.AreEqual(3, Kata.CountBits(7));
+            Assert.AreEqual(2, Kata.CountBits(9));
+            Assert.AreEqual(2, Kata.CountBits(10));
+        }
+    }
+}
---
+++ BitCounting/BitCounting/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BitCounting
+{    public class Kata
+    {
+        public static int CountBits(int n)
+        {
+            string binary = Convert.ToString(n, 2);
+            var binaryArray = binary.ToCharArray();
+            return binaryArray.Where(q => q == '1').Count();
+        }
+    }
+}
